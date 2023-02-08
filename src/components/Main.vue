91fa37dec1363fe1ce08a4a43a0cef4e58dec65d
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-02-07 23:32:16 -0600

    Search

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MainSearch from './MainSearch.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <MainSearch />
+    <h1>Hello There</h1>
+  </div>
+</template>
+
+<style scoped lang="postcass">
+    .wrapper {
+    @apply flex flex-col container mx-auto py-4 gap-4;
+}
+</style>
---
+++ src/components/MainSearch.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">
+    <input type="text" placeholder="Search Staff Member" class="search" />
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.wrapper {
+  @apply relative flex w-full items-stretch;
+  .search {
+    @apply px-3 py-4 placeholder-slate-400;
+  }
+}
+</style>
