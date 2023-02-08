ac9c7e8e1dd759c7ac09e47a5212f8403ac11d65
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-02-07 21:25:31 -0600

    Configure NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,5 +4,18 @@ const brand = ref('RNG Company Directory')
 </script>
 
 <template>
-  <h1>Hello There</h1>
+  <nav class="bg-yellow-500">
+    <div class="wrapper">
+      <div class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </div>
+      <div class="menu">
+        <a href="#" class="menu-item">Depratments </a>
+        <a href="#" class="menu-item">Settings </a>
+        <a href="#" class="menu-item">Logout </a>
+      </div>
+    </div>
+  </nav>
 </template>
+
+<style scoped lang="postcss"></style>
