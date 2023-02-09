cbbf7a3e62345ab88e4a9c017105f17bd1d62656
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-02-08 21:42:12 -0600

    Added Page Select

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,12 +2,14 @@
 <script setup>
 import MainSearch from './MainSearch.vue'
 import MainCards from './MainCards.vue'
+import MainPagination from './MainPagination.vue'
 </script>
 
 <template>
   <div class="wrapper">
     <MainSearch />
     <MainCards />
+    <MainPagination />
   </div>
 </template>
 
