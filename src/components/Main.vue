4aa37e2b0f147dd80e105d4426682c86be273349
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-02-08 20:01:11 -0600

    sub-wrapper

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,18 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
 import MainSearch from './MainSearch.vue'
+import MainCards from './MainCards.vue'
 </script>
 
 <template>
   <div class="wrapper">
     <MainSearch />
-    <h1>Hello There</h1>
+    <MainCards />
   </div>
 </template>
 
 <style scoped lang="postcss">
 .wrapper {
-  @apply flex flex-col gap-4 py-4;
+  @apply container mx-auto flex flex-col gap-4 py-4;
 }
 </style>
---
+++ src/components/MainCardsSingle.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <span class="inline bg-red-500">I'm a Card</span>
+  </div>
+</template>
