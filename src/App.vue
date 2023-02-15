448b55fe22ece063dec0aa95f008a26ea6d9a55f
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-02-14 20:33:12 -0600

    undo the changes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
 import MainPage from '@/components/MainPage.vue'
-
-import { ref } from 'vue'
-const showMainPage = ref(false)
 </script>
 
 <template>
   <NavBar />
-  <button class="bg-orange-700 px-4 py-2" @click="showMainPage = !showMainPage">Show Main Page</button>
-  <MainPage v-if="showMainPage" />
-  <div v-else>
-    <h1>This is another "Page"</h1>
-  </div>
+  <MainPage />
 </template>
