33a4ba3da8024da0ea1b9b4d147052388fbde3b0
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-04-24 20:41:10 -0500

    54 min

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -1,17 +1,21 @@
 <script setup>
-import { ref } from 'vue'
-const pages = ref(10)
-const activePage = ref(4)
-
-const prevPage = () => {
+import useAPI from '@/composables/useAPI'
+const { activePage, pages, getEmployees } = useAPI()
+const prevPage = async () => {
   if (activePage.value > 1) {
     activePage.value--
+    await getEmployees()
   }
 }
-const nextPage = () => {
+const nextPage = async () => {
   if (activePage.value < pages.value) {
     activePage.value++
+    await getEmployees()
   }
+}
+const jumpPage = async (page) => {
+  activePage.value = page
+  await getEmployees()
 }
 </script>
 
